38da890b5644d92583e0a26805daeba3ee8f9772
ElGalloxd <98571275+ElGalloxd@users.noreply.github.com>
2022-02-14 18:45:56 -0500

    Add files via upload

---
+++ -paginaParaDescargar/code.js
@@ -0,0 +1,6 @@
+ "use strict";
+const boton = document.querySelector(".boton");
+
+boton.addEventListener("click",(e)=>{
+    open("https://download942.mediafire.com/3e9t9d67t5wg/5rxxm5ba49j1n74/ConsolaG+instalador.rar")
+})
